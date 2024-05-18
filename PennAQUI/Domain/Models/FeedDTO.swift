58981e0b369cb9a46ffe6187a19e700d94ec4dac
Peter Kos <sirgraystripe@icloud.com>
2024-05-18 18:30:09 -0400

    DTO/Model layer

---
+++ PennAQUI/Domain/Models/FeedDTO.swift
@@ -0,0 +1,46 @@
+//
+//  FeedDTO.swift
+//  PennAQUI
+//
+//  Created by Peter Kos on 5/18/24.
+//
+//
+
+import Foundation
+
+struct FeedDTO: Codable {
+    var status: String?
+    var data: DataDTO?
+}
+
+/// Also gives back a `debug` obj with `sync` (ISO 8601) timestamp
+/// not in docs, ignoring it for now.
+struct DataDTO: Codable {
+    var idx: Int?
+    var aqi: Int?
+    var time: TimeDTO?
+    var city: CityDTO?
+    var attributions: [AttributionsDTO]?
+    // TODO: Leaving these out for now
+//    var iaqi: IAQIDTO?
+//    var forecast: ForecastDTO?
+}
+
+/// EPA Attributions, not documented
+struct AttributionsDTO: Codable {
+    var url: String?
+    var name: String?
+    var logo: String?
+}
+
+/// Also returns a `location` field, not in docs, empty.
+struct CityDTO: Codable {
+    var name: String?
+    var geo: [Int]?
+    var url: String?
+}
+
+struct TimeDTO: Codable {
+    var s: String?
+    var tz: String?
+}
---
+++ PennAQUI/Domain/Models/Models.swift
@@ -0,0 +1,98 @@
+//
+//  Models.swift
+//  PennAQUI
+//
+//  Created by Peter Kos on 5/18/24.
+//
+//
+
+import Foundation
+
+struct FeedData: Codable {
+    enum Status: String, Codable {
+        case ok
+        case error
+    }
+
+    var status: Status?
+    var cityStationID: Int?
+    var airQualityIndex: Int?
+    var time: Date?
+    var city: City?
+    var stationInfo: [StationInfo]?
+
+    init(feedDTO dto: FeedDTO) {
+        if let status = dto.status {
+            self.status = .init(rawValue: status)
+        }
+
+        guard let data = dto.data else {
+            return
+        }
+        cityStationID = data.idx
+        airQualityIndex = data.aqi
+
+        let formatStyle = Date.FormatStyle()
+            .year(.defaultDigits)
+            .month(.twoDigits)
+            .day(.twoDigits)
+            .hour(.twoDigits(amPM: .omitted))
+            .minute(.twoDigits)
+            .second(.twoDigits)
+            .timeZone(.localizedGMT(.long))
+
+        if let timestamp = data.time?.s,
+           let timeZone = data.time?.tz
+        {
+            time = try? Date("\(timestamp) \(timeZone)", strategy: formatStyle)
+        }
+
+        if let cityDTO = data.city {
+            city = City(cityDTO: cityDTO)
+        }
+
+        if let attributionsDTOs = data.attributions {
+            stationInfo = attributionsDTOs.map { StationInfo(attributionsDTO: $0) }
+        }
+    }
+}
+
+struct City: Codable {
+    var name: String?
+    var lat: Int?
+    var long: Int?
+    var logoURL: URL?
+
+    init(cityDTO dto: CityDTO) {
+        name = dto.name
+        // Assuming lat/long is the order
+        lat = dto.geo?[safe: 0]
+        long = dto.geo?[safe: 1]
+        if let urlString = dto.url,
+           let logoURL = URL(string: urlString)
+        {
+            self.logoURL = logoURL
+        }
+    }
+}
+
+struct StationInfo: Codable {
+    var url: URL?
+    var name: String?
+    var logoURL: URL?
+
+    init(attributionsDTO dto: AttributionsDTO) {
+        if let urlString = dto.url,
+           let url = URL(string: urlString)
+        {
+            self.url = url
+        }
+        name = dto.name
+
+        if let urlString = dto.logo,
+           let url = URL(string: urlString)
+        {
+            logoURL = url
+        }
+    }
+}
